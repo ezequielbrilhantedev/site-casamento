6ffb9f6b37389d3eb64e9033c5a7fa53c8295535
Ribeiro Brilhante <e.ribeiro.brilhante@accenture.com>
2021-11-12 14:03:21 -0300

    Criação da funcionalidade de enviar e-mail

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+// const environment = require(process.env.PASSWORD) 
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+
+const port = 3000;
+
+const user = process.env.SMTP_USER
+const pass = process.env.SMTP_PASS
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+
+app.get('/send', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.umbler.com",
+        port: 587,
+        auth: {user, pass}
+    })
+
+    transporter.sendMail({
+        from: user,
+        to: user,
+        replyTo: "ezequielrb7@mail.com",
+        subject: "Confirmação para o casamento",
+        text: "Olá, estamos muito felizes por confirmar sua presença, muito obrigado!"
+    }).then(info => {
+        res.send(info)
+    }).catch(error => {
+        res.send(error)
+        console.log(error)
+    })
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`));
