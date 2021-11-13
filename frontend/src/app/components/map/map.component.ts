7f4e7c9662076754c5c3992cafce32d25fa7c1a7
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-11-12 23:59:03 -0300

    Adição das informações do local da festa e tbm colocando um marcardor no mapa

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -12,6 +12,10 @@ export class MapComponent implements OnInit {
 
   mapa: any;
 
+  pointMarker: any;
+
+  marker: any;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,10 +24,18 @@ export class MapComponent implements OnInit {
     this.mapa = new Mapboxgl.Map({
       container: 'map-mapbox', // container id
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-38.6533602, -3.7645704], // starting position [lng, lat]
+      // center: [-38.6533602, -3.7645704], // starting position [lng, lat]
+      center: [-38.5585839, -3.7314505], // starting position [lng, lat]
       // @-3.7645704,-38.6533602
-      zoom: 17 // starting zoom
+      zoom: 16 // starting zoom
     });
+
+    this.marker = new Mapboxgl.Marker({
+      color: "#f0630e",
+      draggable: true,
+    }).setLngLat([-38.5585839, -3.7314505])
+      .addTo(this.mapa);;
+
   }
 
 }
